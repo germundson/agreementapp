27d30b9b2e811908196bd42bd98e2618f5324cce
Erik Germundson <ege@ambita.com>
2018-02-20 11:17:54 +0100

    delete script when done...

--- buildandrun.sh
+++ buildandrun.sh
@@ -4,11 +4,11 @@ echo building and starting application
 
 dir=$(pwd)
 
-echo "cd $dir/agreementsoap;mvn clean install;cd application-soap-app;mvn spring-boot:run;rm ../soap.sh" >> soap.sh
+echo "cd $dir/agreementsoap;mvn clean install;cd application-soap-app;mvn spring-boot:run;rm ../../soap.sh" >> soap.sh
 chmod 700 soap.sh
 open -a terminal soap.sh
 echo "Waiting for soap-service to start - waiting 1 min"
-sleep 60
-echo "cd $dir/agreement;mvn clean install;cd application-app;mvn spring-boot:run;rm ../rest.sh" >> rest.sh
+sleep 30
+echo "cd $dir/agreement;mvn clean install;cd application-app;mvn spring-boot:run;rm ../../rest.sh" >> rest.sh
 chmod 700 rest.sh
 open -a terminal rest.sh
