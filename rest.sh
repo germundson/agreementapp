c4bea7428a5a98fc899fc43f0f3d292d1427fba8
Erik Germundson <ege@ambita.com>
2018-02-20 11:12:48 +0100

    update script...

--- rest.sh
+++
@@ -1,2 +0,0 @@
-cd /Users/ege/dev/checkout/git/learing/agreementapp/agreement;mvn clean install;cd application-app;mvn spring-boot:run
-cd /Users/ege/dev/checkout/git/learing/agreementapp/agreement;mvn clean install;cd application-app;mvn spring-boot:run
