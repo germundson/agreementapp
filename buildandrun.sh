e22748fd25a0f264eec8fa55e0b7a53adc18fb6c
Erik Germundson <ege@ambita.com>
2018-02-20 11:20:08 +0100

    changing sleep output...

--- buildandrun.sh
+++ buildandrun.sh
@@ -7,7 +7,7 @@ dir=$(pwd)
 echo "cd $dir/agreementsoap;mvn clean install;cd application-soap-app;mvn spring-boot:run;rm ../../soap.sh" >> soap.sh
 chmod 700 soap.sh
 open -a terminal soap.sh
-echo "Waiting for soap-service to start - waiting 1 min"
+echo "Waiting for soap-service to start - waiting 30 secs"
 sleep 30
 echo "cd $dir/agreement;mvn clean install;cd application-app;mvn spring-boot:run;rm ../../rest.sh" >> rest.sh
 chmod 700 rest.sh
